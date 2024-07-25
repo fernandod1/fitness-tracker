6de74ff066d82405861c959661b7d4d0d6d7d811
fernandod1 <fernandodl@gmail.com>
2024-07-25 18:48:51 +0200

    Some feature tests

--- tests/Feature/ActivityTest.php
+++ tests/Feature/ActivityTest.php
@@ -3,43 +3,64 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+
 use App\Models\Activity;
 use App\Models\ActivityType;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
  
 class ActivityTest extends TestCase
-{  
-    public function test_activity_model_exists_in_db(): void
+{      
+    use RefreshDatabase;
+
+	protected $user;
+    protected $activityType;
+    protected $activity;
+    
+    protected function setUp(): void
     {
-        $activity = Activity::factory()->create(); 
-        $this->assertModelExists($activity);
+        parent::setUp();
+        $this->user = User::factory()->create(); 
+        $this->activityType = ActivityType::factory()->create();
+        $this->activity = Activity::factory()->create();  
     }
 
+    public function test_activity_model_exists_in_db(): void
+    {
+        $this->assertModelExists($this->activity);
+    }
+   
     public function test_activities_mainpage_returns_a_successful_response(): void
     {
         $response = $this->get('/');
+        $response
+            ->assertStatus(200)
+            ->assertSeeText('Last tracks of users', $response->getContent());
+    }
+    public function test_activities_filterpage_returns_a_successful_response_if_not_login(): void
+    {
+        $response = $this->get("/activity/filter?activity_type_id={$this->activityType->id}");
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/login');
+    }
+    public function test_activities_filterpage_returns_a_successful_response_if_login(): void
+    {
+        $response = $this
+            ->actingAs($this->user,'web')
+            ->get("/activity/filter?activity_type_id={$this->activityType->id}");
         $response->assertStatus(200);
     }
 
-    public function test_activities_filterpage_returns_a_successful_response(): void
+    public function test_count_activities_database()
     {
-        $response = $this->get('/filter');
-        if(auth()->check())
-            $response->assertStatus(302);
-        else
-            $response->assertStatus(404);
-    }
-
-    public function test_activities_database()
-    {
-        Activity::factory()->count(4)->create();
-        $totalActivities = Activity::count(); 
-        $this->assertDatabaseCount('activities', $totalActivities);
+        $this->assertDatabaseCount('activities', 1);
     }
 
     public function test_store_new_activity()
-    {
+    {        
         $randName = Str::random(15);
         $data = [
             'user_id' => User::all()->random()->id,
@@ -48,20 +69,33 @@ class ActivityTest extends TestCase
             'name' => $randName,
             'distance' => 76,
             'distance_unit' => 'meters',
-            'elapsed_time' => 15,
+            'elapsed_time' => 15
         ];
-        $response = $this->post('/store', $data);
+        $response = $this
+            ->actingAs($this->user, 'web')
+            ->post('/activity/store',  $data);
+
         $activity = Activity::where('name', $randName)->first();
         $this->assertNotNull($activity);
         $response->assertFound()->assertStatus(302);
-        $this->assertEquals($data['name'], $activity->name);
+        $this->assertEquals(ucfirst($data['name']), $activity->name);
     }
 
+ 
     public function test_if_activities_view_loads_and_contains_text()
     {
+        $this->actingAs($this->user, 'web');
         $response = $this->get('/');
         $response->assertStatus(200);
-        $response->assertSee('Fitness activities records');
+        $response->assertDontSee('Last tracks of users');
     }
 
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        unset($this->user);
+        unset($this->activityType);
+        unset($this->activity);
+    }
 }
+
--- tests/Feature/ApiActivityTest.php
+++ tests/Feature/ApiActivityTest.php
@@ -4,11 +4,26 @@ namespace Tests\Feature;
 
 use Illuminate\Http\Response;
 use Tests\TestCase;
+use App\Models\User;
 use App\Models\Activity;
 use App\Models\ActivityType;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ApiActivityTest extends TestCase
 {
+    use RefreshDatabase;
+	protected $user;
+    protected $activityType;
+    protected $activity;
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->activityType = ActivityType::factory()->create();
+        $this->activity = Activity::factory()->create();  
+    }
+
     public function test_get_activities_data_in_valid_format() {
         $jsonStruct = [
             "activities" => [
@@ -36,33 +51,48 @@ class ApiActivityTest extends TestCase
             ], 
             "success"
         ];
-       
-        $this->json('get', 'api/v1/activities')
-             ->assertStatus(Response::HTTP_OK)
-             ->assertJsonStructure($jsonStruct)
-             ->assertJsonFragment(["success" => true]);           
+        $this->actingAs($this->user)            
+            ->json('get', 'api/v1/activities')            
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure($jsonStruct)
+            ->assertJsonFragment(["success" => true]);           
     }
 
     public function test_store_activity_in_database() {
         $jsonStruct = [
             "activities" => [
+                  "user_id",
                   "activity_type_id", 
                   "activity_date", 
                   "name", 
                   "distance", 
                   "distance_unit", 
                   "elapsed_time", 
-                  "id"
-               ], 
+                  "id",
+                  "user" => [
+                    "id",
+                    "name",
+                    "email",
+                    "email_verified_at",
+                    "created_at",
+                    "updated_at"
+                  ],
+                  "activity_type" => [
+                    "id",
+                    "name"
+                  ]
+               ],
             "success" 
         ]; 
-        $payload = Activity::factory()->create();
+        $payload = $this->activity;
         $payloadArr = $payload->toArray();
         $payloadArr['activity_date'] = date("Y-m-d H:i");
-        $this->json('post', 'api/v1/activities', $payloadArr)
-             ->assertStatus(Response::HTTP_CREATED)
-             ->assertJsonStructure($jsonStruct);
-        $this->assertDatabaseHas('activities', ["name" => $payloadArr['name']]);
+
+        $this->actingAs($this->user)
+            ->json('post', 'api/v1/activities', $payloadArr)
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonStructure($jsonStruct);
+        $this->assertDatabaseHas('activities', ["id" => $this->activity->id]);
     }
 
     public function test_get_activities_by_type_in_valid_format() {
@@ -71,6 +101,7 @@ class ApiActivityTest extends TestCase
                 "data" => [
                     '*' => [
                         "id",
+                        "user_id",
                         "activity_type_id",
                         "activity_date",
                         "name",
@@ -97,33 +128,29 @@ class ApiActivityTest extends TestCase
             "success"
         ];
 
-        $randId = ActivityType::all()->random()->id;
-        $this->json('get', 'api/v1/activities/types/'.$randId)
-        ->assertStatus(Response::HTTP_OK)
-        ->assertJsonStructure($jsonStruct)
-        ->assertJsonFragment(["success" => true]);  
+        $this->actingAs($this->user)
+            ->json('get', 'api/v1/activities/types/'.$this->activityType->id)
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure($jsonStruct)
+            ->assertJsonFragment(["success" => true]);  
     }
 
     public function test_get_activities_by_type_returns_error_if_typeid_not_valid()
     {
-        $this->json('get', 'api/v1/activities/types/9876543210')
-        ->assertStatus(Response::HTTP_BAD_REQUEST)
-        ->assertJsonFragment(["success" => false, "message" => "Fields validation errors"]);
+        $invalidTypeId = 9876543210978676;
+        $this->actingAs($this->user)
+            ->json('get', 'api/v1/activities/types/'.$invalidTypeId)
+            ->assertStatus(Response::HTTP_BAD_REQUEST)
+            ->assertJsonFragment(["success" => false, "message" => "Activity type not valid"]);
     }
 
     public function test_dont_store_activity_in_database_if_some_field_is_missing() {
-        // suposing name field is missing
-        $payload = [
-                  "activity_type_id" => ActivityType::all()->random()->id,
-                  "activity_date" => "2000-12-10 12:10:00",
-                  //"name", 
-                  "distance" => 20, 
-                  "distance_unit" => "kilometers", 
-                  "elapsed_time" => 5000 
-        ];
+        $payloadArr = $this->activity->toArray();
+        $payloadArr['activity_date'] = date("Y-m-d H:i");
+        unset($payloadArr["name"]); // suposing name field missing
 
-        $this->json('post', 'api/v1/activities', $payload)
-             ->assertStatus(Response::HTTP_BAD_REQUEST);
+        $this->actingAs($this->user)
+            ->json('post', 'api/v1/activities', $payloadArr)
+            ->assertStatus(Response::HTTP_BAD_REQUEST);            
     }
-
 }
